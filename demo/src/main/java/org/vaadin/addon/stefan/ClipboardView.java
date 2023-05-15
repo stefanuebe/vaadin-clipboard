ef7ef705751741579a42ee45baf636aeb99eea7f
Stefan Uebe <stefan@vaadin.com>
2023-05-15 13:57:10 +0200

    Updated Vaadin and JAva version. Minor enhancement to demo

--- demo/src/main/java/org/vaadin/addon/stefan/ClipboardView.java
+++ demo/src/main/java/org/vaadin/addon/stefan/ClipboardView.java
@@ -3,6 +3,7 @@ package org.vaadin.addon.stefan;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Pre;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
@@ -31,7 +32,7 @@ public class ClipboardView extends VerticalLayout {
         H1 title = new H1("Clipboard for Vaadin Flow Demo");
         title.getStyle().set("font-size", "1.25em").set("margin-bottom", "0");
         add(title);
-        Span subtitle = new Span("Addon version 1.0.2, built with Vaadin 14.8.14.");
+        Span subtitle = new Span("Addon version 1.0.3, built with Vaadin 14.8.14.");
         subtitle.getStyle().set("color", "gray").set("font-size", "0.8em");
         add(subtitle);
 
@@ -47,11 +48,15 @@ public class ClipboardView extends VerticalLayout {
         readLayout.setFlexGrow(1, output);
         readLayout.setHorizontalComponentAlignment(Alignment.STRETCH, output);
 
+        Pre serverSideSample = new Pre(SAMPLE_TEXT);
+        serverSideSample.getStyle().set("padding", "1rem").set("background", "lightgray").set("border", "1px dashed gray");
 
         // write part
         Button sampleToClipboard = new Button("Copy server side sample to clipboard", event -> {
             ClientsideClipboard.writeToClipboard(SAMPLE_TEXT, this::handleWriteResult);
         });
+
+
 
         TextArea textArea = new TextArea("Write something here to copy to clipboard");
         textArea.setHeight("300px");
@@ -62,8 +67,9 @@ public class ClipboardView extends VerticalLayout {
             ClientsideClipboard.writeToClipboard(textArea.getValue(), this::handleWriteResult);
         });
 
-        VerticalLayout writeLayout = new VerticalLayout(sampleToClipboard, textArea, textareaContentToClipboard);
-        writeLayout.setHorizontalComponentAlignment(Alignment.STRETCH, textArea);
+        Hr hr = new Hr();
+        VerticalLayout writeLayout = new VerticalLayout(serverSideSample, sampleToClipboard, hr, textArea, textareaContentToClipboard);
+        writeLayout.setHorizontalComponentAlignment(Alignment.STRETCH, textArea, hr, serverSideSample);
 
         // main layout
         HorizontalLayout mainLayout = new HorizontalLayout(readLayout, writeLayout);
